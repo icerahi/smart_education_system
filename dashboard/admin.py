75c1fd1f8ff623655968e8fed5f10a6e707987a1
rahi <zanjarwhite@gmail.com>
2020-10-18 10:40:11 +0600

    done

---
+++ dashboard/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+admin.site.site_title = "Smart Education System"
+admin.site.site_header = "Smart Education System"
+admin.site.site_index_title = "Smart Education System"
+
