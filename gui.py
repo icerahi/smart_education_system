937b5afcdd8c99c5c59d1d2c2b9b25c8f22dae41
rahi <zanjarwhite@gmail.com>
2020-11-06 02:00:56 +0600

    test to view pywebview

---
+++ gui.py
@@ -0,0 +1,22 @@
+import webview
+import os
+import threading
+from time import sleep
+def server_start():
+    os.system('python manage.py runserver')
+    
+
+def server_close():
+    os.system('pkill -f runserver')
+
+
+t1 = threading.Thread(target=server_start)
+t1.start()
+sleep(1)
+
+if __name__ == '__main__':
+
+    window = webview.create_window('Simple browser', 'http://localhost:8000',width=1280,height=700)
+    window.closing+=server_close
+    
+    webview.start()
